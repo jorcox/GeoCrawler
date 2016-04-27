b40bec05afb3722ebf27312b843cc9c65ab32bf2
Jorge <646122@unizar.es>
2016-04-27 22:41:44 +0200

    Nuevos temas ingresados al tesauro

--- src/plugin/scoring-geo/src/java/org/unizar/nutch/scoring/geo/thesaurus/Thesaurus.java
+++ src/plugin/scoring-geo/src/java/org/unizar/nutch/scoring/geo/thesaurus/Thesaurus.java
@@ -18,6 +18,14 @@ import java.io.FileNotFoundException;
 public class Thesaurus {
 
 	private static final String themeGeo = "<http://www.eionet.europa.eu/gemet/theme/16>";
+	private static final String themeAir = "<http://www.eionet.europa.eu/gemet/theme/3>";
+	private static final String themeClimate = "<http://www.eionet.europa.eu/gemet/theme/7>";
+	private static final String themeAgricultute = "<http://www.eionet.europa.eu/gemet/theme/2>";
+	private static final String themeFishery = "<http://www.eionet.europa.eu/gemet/theme/12>";
+	private static final String themeNatural = "<http://www.eionet.europa.eu/gemet/theme/23>";
+	private static final String themePollution = "<http://www.eionet.europa.eu/gemet/theme/26>";
+	private static final String themeWater = "<http://www.eionet.europa.eu/gemet/theme/40>";
+	private static final String themeUrban = "<http://www.eionet.europa.eu/gemet/theme/38>";
 	private static final String uriGEMET = "http://www.eionet.europa.eu/gemet/2004/06/gemet-schema.rdf#";
 	private Model model;
 	private static String[] langs = { "ar", "bg", "cs", "da", "de", "el", "en-US", "en", "es", "et", "eu", "fi", "fr", "hu",
@@ -41,7 +49,15 @@ public class Thesaurus {
 		String queryString = "PREFIX rdf: <" + RDF.getURI() + "> PREFIX rdfs: <" + RDFS.getURI() + "> PREFIX skos: <"
 				+ SKOS.getURI() + "> PREFIX gemet: <" + uriGEMET + "> "
 				+ "SELECT distinct ?x WHERE { ?y rdf:type skos:Concept . ?y skos:prefLabel ?x . FILTER regex(?x, '"
-				+ word + "', 'i') ." + " ?y gemet:theme " + themeGeo + "}";
+				+ word + "', 'i') ." + "{ ?y gemet:theme " + themeGeo + "} UNION {" + 
+				"?y gemet:theme " + themeAir + "} UNION {" +
+				"?y gemet:theme " + themeClimate + "} UNION {" +
+				"?y gemet:theme " + themeAgricultute + "} UNION {" +
+				"?y gemet:theme " + themeFishery + "} UNION {" +
+				"?y gemet:theme " + themeNatural + "} UNION {" +
+				"?y gemet:theme " + themePollution + "} UNION {" +
+				"?y gemet:theme " + themeWater + "} UNION {" +
+				"?y gemet:theme " + themeUrban + "}}";
 
 		Query query = QueryFactory.create(queryString);
 		int c = 0;
@@ -60,7 +76,7 @@ public class Thesaurus {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		Thesaurus th = new Thesaurus();
-		th.execQuery("Afrika");
+		th.execQuery("agua");
 		/*
 		 * Model model1 = FileManager.get().loadModel("gemetThesaurus.rdf");
 		 * Model model2 = FileManager.get().loadModel("gemet-backbone.rdf");
